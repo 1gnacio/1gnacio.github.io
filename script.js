ec75a4221b69d5bf0d4c9d76257fd480bed563bf
1gnacio <ignacio5garcia@hotmail.com>
2023-01-24 15:49:53 -0300

    initial release

--- script.js
+++ script.js
@@ -4,6 +4,49 @@ document.addEventListener("DOMContentLoaded", function() {
         var name = document.getElementById("name").value;
         var email = document.getElementById("email").value;
         var message = document.getElementById("message").value;
+        var hasErrors = false;
+        var validElements = document.getElementsByClassName("is-valid");
+        var invalidElements = document.getElementsByClassName("is-invalid");
+
+        while (validElements.length > 0) {
+            validElements[0].classList.remove("is-valid");
+        }
+
+        while (invalidElements.length > 0) {
+            invalidElements[0].classList.remove("is-invalid");
+        }
+
+        if (name == ""){
+            document.getElementById("name-validation").style.display = "block";
+            document.getElementById("name").classList.add("is-invalid");
+            hasErrors = true;
+        } else {
+            document.getElementById("name-validation").style.display = "none";
+            document.getElementById("name").classList.add("is-valid");
+        }
+
+        if (email == ""){
+            document.getElementById("email-validation").style.display = "block";
+            document.getElementById("email").classList.add("is-invalid");
+            hasErrors = true;
+        } else {
+            document.getElementById("email-validation").style.display = "none";
+            document.getElementById("email").classList.add("is-valid");
+        }
+
+        if (message == ""){
+            document.getElementById("message-validation").style.display = "block";
+            document.getElementById("message").classList.add("is-invalid");
+            hasErrors = true;
+        } else {
+            document.getElementById("message-validation").style.display = "none";
+            document.getElementById("message").classList.add("is-valid");
+        }
+
+        if (hasErrors) {
+            return false;
+        }
+
         var mailto = "mailto:igarcia@fi.uba.ar?subject=New contact form submission from " + name + "&body=" + message + " %0D%0A %0D%0A From: " + email;
         window.location.href = mailto;
     });
@@ -39,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const myCarouselElement = document.querySelector('#carouselExampleCaptions')
 
     const carousel = new bootstrap.Carousel(myCarouselElement, {
-    interval: 2000,
+    interval: 6000,
     touch: false
     })
 });
